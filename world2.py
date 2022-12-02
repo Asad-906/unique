c35a76dc032c00bc6130521784d4d757e0ffebca
Asad Chohan <asadchauhan906@gmail.com>
2022-12-02 17:46:53 +0500

    second commit

---
+++ world2.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+def create_app():
+    app = Flask("abc")
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = 2014  # year
+mm = 11    # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+
